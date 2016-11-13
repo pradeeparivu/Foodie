c79bf17acfaca4157abbb4108c8effd83a594ec2
pradeep arivu <pradeep arivu>
2016-11-13 19:51:29 +0530

    initial check-in

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+function sele(){
+            var stateid = document.getElementById('cities').value;
+
+    window.location.href = stateid + '.html';
+
+
+            }
+
+function navtomain(){
+        window.location.href='index.html';
+    }
